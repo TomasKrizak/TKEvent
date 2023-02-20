753c902eece386b4d8b9c0bce08bbf43ecddcbd9
TomasKrizak <117197674+TomasKrizak@users.noreply.github.com>
2023-02-20 21:45:22 +0100

    Add files via upload

---
+++ RED_to_TK/install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash  
+
+	echo "                                          "
+	echo "Please enter the full path to *TKEvent* root folder:"
+	read TKEVENT_P
+	echo " "
+
+	rm -rf build
+	mkdir build
+
+	cd build
+	
+	cmake -DTKEvent_PATH=$TKEVENT_P ..
+
+	make
