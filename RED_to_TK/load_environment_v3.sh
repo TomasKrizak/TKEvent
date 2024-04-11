375c547b6f3d1bded50fff337e06199e8faca625
Tomas KRIZAK <tkrizak@cca017.in2p3.fr>
2024-04-11 14:38:57 +0200

    new runs converting fixed

--- RED_to_TK/load_environment_v3.sh
+++ RED_to_TK/load_environment_v3.sh
@@ -1,5 +1,5 @@
 #!/bin/bash 
 
-source /sps/nemo/scratch/chauveau/software/gitlab-snfee-new/this_snfee.sh
+source /sps/nemo/scratch/chauveau/software/gitlab-snfee-goliviero/this_snfee.sh
 
 export RED_PATH=/sps/nemo/snemo/snemo_data/raw_data/RED
